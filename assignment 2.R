3c161f7b398fe6994358dfaccf3f608b5d85096b
Danielle Janney <77296724+djanney1@users.noreply.github.com>
2021-01-21 14:22:41 -0500

    Update assignment 2.R

--- assignment 2.R
+++ assignment 2.R
@@ -5,8 +5,10 @@ myMean <- function(assignment2) { return(sum(assignment)/length(someData)) }
    # Right away I see there will be an error because "assignment" and "someData"
    # have not been given any values
    # Test function:
-myMean(assignment2)
+myMean(assignment2) 
+   # Returns "Error in myMean(assignment2) : object 'assignment' not found"
    # If the mean of "assignment2" is the intention of the function, 
    # then insert "assignment2" in both sum() and length()
 myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
-myMean(assignment2) #returns mean of "assignment2"
+myMean(assignment2) # Returns mean of "assignment2"
+[1] 19.25
