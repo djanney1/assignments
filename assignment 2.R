b9d77be0409ae21f3814d8afc0a82a4ac63ff5a9
Danielle Janney <77296724+djanney1@users.noreply.github.com>
2021-01-21 14:14:53 -0500

    Add files via upload

---
+++ assignment 2.R
@@ -0,0 +1,12 @@
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22) 
+   # Input values for "assignment2"
+myMean <- function(assignment2) { return(sum(assignment)/length(someData)) }
+   # Input function for testing
+   # Right away I see there will be an error because "assignment" and "someData"
+   # have not been given any values
+   # Test function:
+myMean(assignment2)
+   # If the mean of "assignment2" is the intention of the function, 
+   # then insert "assignment2" in both sum() and length()
+myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
+myMean(assignment2) #returns mean of "assignment2"
