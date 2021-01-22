4e7373f23321148425006767e3fa908f718fdc59
Danielle Janney <77296724+djanney1@users.noreply.github.com>
2021-01-22 13:18:44 -0500

    Update assignment 2.R

--- assignment 2.R
+++ assignment 2.R
@@ -1,7 +1,7 @@
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22) 
-   # Input values for "assignment2"
+   # Input given values for "assignment2"
 myMean <- function(assignment2) { return(sum(assignment)/length(someData)) }
-   # Input function for testing
+   # Input given function for testing
    # Right away I see there will be an error because "assignment" and "someData"
    # have not been given any values
    # Test function:
