e71bdd04e27a24449cb61e6d29d38d34082d760e
Danielle Janney <77296724+djanney1@users.noreply.github.com>
2021-03-01 14:05:55 -0500

    Add files via upload

---
+++ assignment 5.R
@@ -0,0 +1,12 @@
+# given values for matrices
+A <- matrix(1:100, nrow=10)
+B <- matrix(1:1000, nrow=10)
+d# transpose matrix A
+Atran <- matrix(1:100, nrow=10, byrow=T) #or
+Atran <- t(A)
+# randomize values in matrix A
+A2 <- matrix(sample(A),nrow=10)
+# assign randomized matrix
+A3 <- matrix(A2,nrow=10)
+# transpose matrix A3
+t(A3)
